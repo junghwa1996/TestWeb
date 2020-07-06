1f65d37ee934861226e9105f5a189dc6fac1d162
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2020-07-06 22:03:02 +0900

    Update fixd.js

--- RRR/js/fixd.js
+++ RRR/js/fixd.js
@@ -1,13 +1,25 @@
 $(function(){
     searchopen();
+    navFixed();
 });
 
 function searchopen(){
     $('.search_ico').click(function(){
         $('.search input[type="text"]').toggleClass('open');
     });
-    $('.All_view').click(function(){
-        $('.All_Menu').toggleClass('All_open');
-    });
 }
 // search ico 클릭시 검색란 오픈
+
+
+
+
+function navFixed(){
+    var nav = $('.header_Wrap').height();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > nav){
+            $('.Fixd_Nav').addClass('fixed');
+        }else{
+            $('.Fixd_Nav').removeClass('fixed');
+        }
+    });
+}
