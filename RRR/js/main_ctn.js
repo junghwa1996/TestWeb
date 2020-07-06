616137035479f86d07ea505bd9967408b29e20b1
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2020-07-06 22:03:17 +0900

    Update main_ctn.js

--- RRR/js/main_ctn.js
+++ RRR/js/main_ctn.js
@@ -1,6 +1,8 @@
 $(function(){
     MainSlider();
     BannerSlide();
+    swiper();
+    text_Banner_slider();
 });
 
 function MainSlider(){
@@ -11,7 +13,43 @@ function MainSlider(){
 }
 function BannerSlide(){
     $('.BannerSlide').bxSlider({
-        speed:'300',
-        auto:'true'
     });
+}
+function text_Banner_slider(){
+    $('.text_Banner_slider').bxSlider({
+        speed:'400',
+        auto:'true',
+        mode:'fade',
+    });
+}
+
+
+function swiper(){
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 50,
+        slidesPerGroup: 4,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        centereSlides:true,
+        autoplay:{
+            delay: 2500,
+            disbleOnInteraction: false,
+        },
+    });
+    $( ".swiper-container" ).hover(
+        function() {
+          swiper .autoplay.stop();
+        }, function() {
+          swiper .autoplay.start();
+        }
+      );
 }
